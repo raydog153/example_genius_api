d42c1efefbb6c5bb28d2ecf4008ee2d4091b5112
raydog153 <ray.boutotte@gmail.com>
2023-01-22 22:38:58 -0500

    Fixing search logic for multiple pages (#1)
    * Fixing search logic for multiple pages
    
    Co-authored-by: Ray Boutotte <rayboutotte@Rays-MacBook-Pro-2.local>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,75 @@ class HomeController < ApplicationController
   end
 
   def fetch_artist_songs
-    artist = params[:artist]
-    songs = Genius::Song.search(artist)
-    @songs = songs.select do |song|
-      # Only add songs if the artist matches up
-      song.primary_artist.name == artist
-    end
+    # Two different approaches were found, leaving both in for discussion on both ways...
+    @songs = artist_songs_by_artist_id
+    # @songs = artist_songs_by_search
 
     render "home/song_list"
-  rescue Genius::AuthenticationError => e
-    if Genius.access_token.blank?
-      @error_message = "Your access token is blank, please check..."
+  rescue Genius::AuthenticationError
+    @error_message = if Genius.access_token.blank?
+      I18n.t("errors.access_token_blank")
     else
-      @error_message = "Your access token is invalid, please check..."
+      I18n.t("errors.access_token_invalid")
     end
     render "layouts/auth_error"
+  end
+
+  private
+
+  def artist_songs_by_artist_id
+    artist = nil
+    songs_by_artist = []
+    page = 0
+
+    # Search to find the artist first
+    search_results = Genius::Song.search(params[:artist])
+    search_results.each do |result|
+      # Only add songs if the artist matches up
+      if result.primary_artist.name.downcase == params[:artist].downcase
+        artist = Genius::Artist.find(result.primary_artist.id)
+        break
+      end
+    end
+
+    # Return if no artist found
+    return songs_by_artist unless artist.present?
+
+    # lets find all songs by this artist
+    loop do
+      songs = artist.songs(params: {per_page: 20, page: (page += 1)})
+      songs_by_artist += songs.select do |song|
+        # Only add songs if the artist name matches up
+        song.primary_artist.name == artist.name
+      end
+
+      # Ideally we should break if count is less that per_page,
+      # however for some reason often API pages are not always full
+      break if songs.count == 0
+    end
+    songs_by_artist
+  end
+
+  def artist_songs_by_search
+    songs_by_artist = []
+    page = 0
+    artist = params[:artist].downcase
+
+    # Use search API to find all songs by artist
+    loop do
+      songs = Genius::Song.search(artist, params: {per_page: 20, page: (page += 1)})
+      songs_by_artist += songs.select do |song|
+        # Only add songs if the artist name matches up
+        song.primary_artist.name.downcase == artist
+      end
+
+      # Ideally we should break if count is less than per_page,
+      # however for some reason often API pages are not always full
+      break if songs.count == 0
+    end
+
+    # Sort sing list and return
+    songs_by_artist.sort_by { |song| song.title }
   end
 
 end
